dc0601e480b146578ea6489f0a0b46d0a1d4c3ce
Cleber Teodoro dos Santos <cleber.santos@betha.com.br>
2020-10-18 13:29:40 -0200

    '.'

--- siconfi-extrato-entrega.js
+++ siconfi-extrato-entrega.js
@@ -1,11 +1,10 @@
 // npm install axios => instar o axios por meio do node.
 const axios = require('axios');
 
-for(it of [4204251,4208906]){ console.log(it)};
-
-
-// for(ibge of [4204251,4208906]){
-    let ibge = 4204251
+// for(it of [4204251,4208906]){ console.log(it)};
+const listCode = [4204251,4208906]
+// let ibge = 4204251
+listCode.forEach(ibge => {
     const rgf = registro => registro.entregavel == 'Relatório de Gestão Fiscal';
     const parametros = {
         "id_ente" : ibge ,
@@ -22,14 +21,15 @@ for(it of [4204251,4208906]){ console.log(it)};
     console.log(">>>>>>>>>>>>>>>>>>>>   DADOS SICONFI   <<<<<<<<<<<<<<<<<<<<")
     console.log(`API : ${url}`)
     // const rgf = registro => registro.entregavel == 'Relatório de Gestão Fiscal' | registro.entregavel == 'Relatório Resumido de Execução Orçamentária' ;
-    axios(apiSICONFI)
-    .then(resp => {
-        for(it of resp.data.items.filter(rgf)){
-            // console.log(it);
-            console.log(JSON.stringify(it));
-            break;
-        }
-
-    })
-    .catch(err => console.log(err))
-// }
+    axios.get(url).then(resp => console.log(resp)).catch(err => console.log(err))
+    // axios(apiSICONFI)
+    // .then(resp => {
+    //     for(it of resp.data.items.filter(rgf)){
+    //          console.log(it);
+    //         // console.log(JSON.stringify(it));
+    //         break;
+    //     }
+    // })
+    // .catch(err => console.log(err))
+});
+    
