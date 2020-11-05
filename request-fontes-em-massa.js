e8de85c38c20b6d943c13678d636bc5ff4ef8d8e
Cleber Teodoro dos Santos <cleber.santos@betha.com.br>
2020-11-05 00:02:32 -0300

    Alterado o arquivo js/fontes-dados e criado o arquivo resquest-fontes-em-massa

---
+++ request-fontes-em-massa.js
@@ -0,0 +1,51 @@
+// npm install axios => instar o axios por meio do node.
+const fontesDados = require("./js/fontes-dados")
+const axios = require('axios');
+const base64 = require('base-64')
+const exercicio = "{\"exercicio\":2020}";
+const headers = {
+    'app-context' : base64.encode(exercicio),
+    'authorization' : fontesDados.token,
+    'user-access': fontesDados.entidades.cocaldosul
+}
+const parametros = {
+    "limit" : 1, // podemos jogar uma paginação de até 10000
+    "offset" : 0
+}
+
+const imprimir = (param) => `[${param.tipo}] -> ${param.url.split('/')[param.url.split('/').length -1]} => ${param.conteudo}`
+
+function getConsultFontData(url) {
+    return new Promise((resolve,reject) => {
+        const fonteDadosConsulta = {
+            url : url ,
+            method: 'get',
+            params: parametros,
+            headers : headers
+        }
+        try{
+            axios(fonteDadosConsulta)
+                .then(resp => {
+                    // console.log(resp);
+                    if(resp.status == 200 || resp.status == 201){
+                        resolve(imprimir({tipo : 'SUCESSO',url : url, conteudo : JSON.stringify(resp.data.content).substring(0,30)}))
+                    }else{
+                        reject(imprimir({tipo : 'ERRO',url : url, conteudo : JSON.stringify(resp.data.content)}))
+                    }
+                    // let fonteDados = resp.data.content;
+                    // console.log(fonteDados);
+                })
+                // .catch(err => imprimir({tipo : 'ERRO',url : url}))
+                .catch(err => console.log(`[ERRO] -> ${url.split('/')[url.split('/').length -1]}`))
+        }catch(e){
+            console.log(`[ERRO] -> ${url.split('/')[url.split('/').length -1]}`)
+        }
+    });
+}
+
+for(it of fontesDados.temasFontesDados){
+    getConsultFontData(it)
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
+    //break;
+}
